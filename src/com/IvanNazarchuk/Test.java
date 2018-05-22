6dd324862cc3c882f07f08e26b165aca8cd8b267
Ivan <nazarchukvan9@gmail.com>
2018-05-22 21:57:49 +0300

    clear

--- src/com/IvanNazarchuk/Test.java
+++ src/com/IvanNazarchuk/Test.java
@@ -2,7 +2,6 @@ package com.IvanNazarchuk;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("New change");
+
     }
 }
